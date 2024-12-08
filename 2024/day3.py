80703e1c47a3d7e90ba89572faedf86efc9866c2
JeronimoLa <81096816+JeronimoLa@users.noreply.github.com>
2024-12-07 18:33:13 -0600

    refactor: change's made to part1 and part2 completed

--- 2024/day3.py
+++ 2024/day3.py
@@ -1,17 +1,42 @@
 import re
+from functools import reduce
+from operator import mul
+
 
 search_pattern = r'mul\([0-9]{0,3},[0-9]{0,3}\)'
 
+def part1(data:list) ->int:
+    formatted = "".join(data)
+    found = re.findall(search_pattern, formatted)
+    ans = [ reduce(mul,map(int, re.findall(r'\d+', item))) for item in found ]
+    return sum(ans)
+
+def part2(data:list):
+    keyword = "do()"
+    enable_instructions, temp = [], []
+    index = 0
+
+    while len(data) > index:            
+        if data[index] == list(keyword)[0]:
+            if enable_instructions == []:
+                enable_instructions.append(temp)
+            temp = []
+        temp.append(data[index])
+
+        if data[index:index+4] == list(keyword):
+            enable_instructions.append(temp)
+
+        index += 1
+    total = [ reduce(mul, map(int,re.findall(r'\d+', c))) for instruction_list in enable_instructions for c in re.findall(search_pattern, "".join(instruction_list)) ]
+    return sum(total)
+
 def main():
-    total = []
-    with open("data/3.txt") as f:
-        for item in re.findall(search_pattern, f.read()):
-            x, y = re.findall(r'\d+', item)
-            total.append(int(x) * int(y))
-        print(sum(total))
+    data = [ item for item in open("data/3.txt").read() ]
+    ans = part1(data)
+    print(f"Part 1: {ans}")
 
-
-
+    ans2 = part2(data)
+    print(f"Part 2: {ans2}")
+    
 if __name__ == "__main__":
     main()
-        
