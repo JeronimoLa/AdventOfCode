c524c5d99878891cb88729c898e1b4876221af9a
JeronimoLa <81096816+JeronimoLa@users.noreply.github.com>
2024-12-09 21:37:36 -0600

    refactor: finished part1

--- 2024/day4.py
+++ 2024/day4.py
@@ -8,16 +8,36 @@ def part1(data:str) -> list:
     ans = 0
     for i in range(R):
         for j in range(C):
-            try:
-                if j < C - 3 and data[i][j] == 'X' and data[i][j+1] == 'M' and data[i][j+2] == 'A' and  data[i][j+3] == 'S':
-                    ans += 1
-                
-            except IndexError:
-                continue
+            # front
+            if j < C - 3 and data[i][j] == 'X' and data[i][j+1] == 'M' and data[i][j+2] == 'A' and  data[i][j+3] == 'S':
+                ans += 1
+            # back
+            if j > 2 and data[i][j] == 'X' and data[i][j-1] == 'M' and data[i][j-2] == 'A' and  data[i][j-3] == 'S':
+                ans += 1
+            # down
+            if i < R - 3 and data[i][j] == 'X' and data[i+1][j] == 'M' and data[i+2][j] == 'A' and  data[i+3][j] == 'S':
+                ans += 1
+            # up
+            if i > 2 and data[i][j] == 'X' and data[i-1][j] == 'M' and data[i-2][j] == 'A' and  data[i-3][j] == 'S':
+                ans += 1
+            # diag left down 
+            if i < R - 3 and j > 2 and data[i][j] == 'X' and data[i+1][j-1] == 'M' and data[i+2][j-2] == 'A' and  data[i+3][j-3] == 'S':
+                ans += 1
+            # diag right up
+            if i > 2 and j < C - 3 and data[i][j] == 'X' and data[i-1][j+1] == 'M' and data[i-2][j+2] == 'A' and  data[i-3][j+3] == 'S':
+                ans += 1
+            # diag right down
+            if i < R - 3 and j < C - 3 and data[i][j] == 'X' and data[i+1][j+1] == 'M' and data[i+2][j+2] == 'A' and  data[i+3][j+3] == 'S':
+                ans += 1
+            # diag left up 
+            if j > 2 and i > 2 and data[i][j] == 'X' and data[i-1][j-1] == 'M' and data[i-2][j-2] == 'A' and  data[i-3][j-3] == 'S':
+                ans += 1 
+    
     return ans
 
 def main():
-    data = open("sample_data/4.txt").read().strip().split("\n")
+    data = open("data/4.txt").read().strip().split("\n")
+    
     ans = part1(data)
     print(ans)
 
